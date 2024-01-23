4343b62d9fbc8e75fd3dc1324d1a65bb03b17e79
fxrest704 <fxrest704@gmail.com>
2024-01-23 20:43:56 +0900

    Initial Commit

---
+++ SwiftToDoList/SwiftToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftToDoListApp.swift
+//  SwiftToDoList
+//
+//  Created by 밀가루 on 1/23/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
