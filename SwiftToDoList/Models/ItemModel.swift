73fa20b9fee22e7327e39d7719bd1277f2076bf6
wxxd-fxrest <fxrest704@gmail.com>
2024-01-24 22:52:00 +0900

    SwiftUI: Todo List

---
+++ SwiftToDoList/Models/ItemModel.swift
@@ -0,0 +1,8 @@
+//
+//  ItemModel.swift
+//  SwiftToDoList
+//
+//  Created by 밀가루 on 1/24/24.
+//
+
+import Foundation
---
+++ SwiftToDoList/Views/ListRowView.swift
@@ -0,0 +1,18 @@
+//
+//  ListRowView.swift
+//  SwiftToDoList
+//
+//  Created by 밀가루 on 1/24/24.
+//
+
+import SwiftUI
+
+struct ListRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListRowView()
+}
