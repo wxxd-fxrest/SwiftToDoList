c898e4f2864bfcaf20d01c51e2eb9b6624e8aa81
wxxd-fxrest <fxrest704@gmail.com>
2024-01-26 15:04:30 +0900

    SwiftUI: END

---
+++ SwiftToDoList/ViewModels/ListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ListViewModel.swift
+//  SwiftToDoList
+//
+//  Created by 밀가루 on 1/25/24.
+//
+
+import Foundation
---
+++ SwiftToDoList/Views/NoItemsVIew.swift
@@ -0,0 +1,18 @@
+//
+//  NoItemsVIew.swift
+//  SwiftToDoList
+//
+//  Created by 밀가루 on 1/25/24.
+//
+
+import SwiftUI
+
+struct NoItemsVIew: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NoItemsVIew()
+}
